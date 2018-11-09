890b429db578d92b42d0a66daa5d4843e631fcf1
David M <mullaned@gmail.com>
2018-11-09 20:54:32 +0000

    api setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,121 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="now-playing-list">
+    <div v-for="genre in filmGenres">
+      <p>{{genre}}</p>
+    </div>
+    <div v-for="movie in moviesData">
+      <h3>{{movie.title}}</h3>
+      <img class="poster-image" :src="movie.poster_path" alt="">
+      
+      <h4 v-for="genre in movie.genre_ids">{{genre}}</h4>
+    </div>
   </div>
 </template>
 
+
 <script>
+import axios from "axios";
+
 export default {
-  name: 'HelloWorld',
+  name: "search",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      moviesData: [],
+      filmGenres: []
+    };
+  },
+  created() {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=4f7f99180a8a7100e411b2d21d0a90e3&language=en-US&page=1"
+      )
+      .then(res => {
+        const moviesData = res.data.results;
+        const filmGenres = [];
+        for (let i = 0; i < moviesData.length; i++) {
+          //concat the image path prefix to the poster paths
+          moviesData[i].poster_path =
+            "https://image.tmdb.org/t/p/w500" + moviesData[i].poster_path;
+
+          //translate the genre id to text and add the listed genres to the filmGenres array
+          for (let j = 0; j < moviesData[i].genre_ids.length; j++) {
+            if (moviesData[i].genre_ids[j] === 28) {
+              moviesData[i].genre_ids[j] = "Action";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 12) {
+              moviesData[i].genre_ids[j] = "Adventure";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 16) {
+              moviesData[i].genre_ids[j] = "Animation";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 35) {
+              moviesData[i].genre_ids[j] = "Comedy";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 80) {
+              moviesData[i].genre_ids[j] = "Crime";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 99) {
+              moviesData[i].genre_ids[j] = "Documentary";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 18) {
+              moviesData[i].genre_ids[j] = "Drama";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 10751) {
+              moviesData[i].genre_ids[j] = "Family";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 14) {
+              moviesData[i].genre_ids[j] = "Fantasy";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 36) {
+              moviesData[i].genre_ids[j] = "History";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 27) {
+              moviesData[i].genre_ids[j] = "Horror";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 10402) {
+              moviesData[i].genre_ids[j] = "Music";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 9648) {
+              moviesData[i].genre_ids[j] = "Mystery";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 10749) {
+              moviesData[i].genre_ids[j] = "Romance";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 878) {
+              moviesData[i].genre_ids[j] = "Science Fiction";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 10770) {
+              moviesData[i].genre_ids[j] = "TV Movie";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 53) {
+              moviesData[i].genre_ids[j] = "Thriller";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 10752) {
+              moviesData[i].genre_ids[j] = "War";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            } else if (moviesData[i].genre_ids[j] === 37) {
+              moviesData[i].genre_ids[j] = "Western";
+              filmGenres.push(moviesData[i].genre_ids[j]);
+            }
+          }
+        }
+        //remove duplicates from the array
+        let uniqueFilmGenres = [...new Set(filmGenres)];
+
+        this.filmGenres = uniqueFilmGenres;
+        this.moviesData = moviesData;
+        console.log(moviesData);
+      })
+      .catch(error => console.log(error));
+  },
+
+  methods: {
+    
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -54,5 +133,8 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.poster-image {
 }
 </style>
